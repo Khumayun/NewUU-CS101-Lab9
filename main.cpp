42526c14d0004c5ffe3f3eb45e3750bc49458f95
Khumayun <k.aminaddinov@gmail.com>
2024-04-24 10:15:35 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,63 @@
+//
+// Created by ...
+//
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    cout << "PROBLEM 1\n";
+
+    cout << "PROBLEM 2\n";
+
+    cout << "PROBLEM 3\n";
+
+    cout << "PROBLEM 4\n";
+
+    cout << "PROBLEM 5\n";
+
+    cout << "PROBLEM 6\n";
+
+    cout << "PROBLEM 7\n";
+
+    cout << "PROBLEM 8\n";
+
+    cout << "PROBLEM 9\n";
+
+    cout << "PROBLEM 10\n";
+
+    cout << "PROBLEM 11\n";
+
+    cout << "PROBLEM 12\n";
+
+    cout << "PROBLEM 13\n";
+
+    cout << "PROBLEM 14\n";
+
+    cout << "PROBLEM 15\n";
+
+    cout << "PROBLEM 16\n";
+
+    cout << "PROBLEM 17\n";
+
+    cout << "PROBLEM 18\n";
+
+    cout << "PROBLEM 19\n";
+
+    cout << "PROBLEM 20\n";
+
+    cout << "PROBLEM 21\n";
+
+    cout << "PROBLEM 22\n";
+
+    cout << "PROBLEM 23\n";
+
+    cout << "PROBLEM 24\n";
+
+    cout << "PROBLEM 25\n";
+
+    return 0;
+}
