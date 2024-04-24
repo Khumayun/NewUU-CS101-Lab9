77fe04e09c857c32414a6bfa9fadca019ec8006a
k.aminaddinov <k.aminaddinov@newuu.uz>
2024-04-24 10:25:51 +0500

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,63 +1,171 @@
-//
-// Created by ...
-//
 #include <iostream>
 #include <cmath>
-#include <cstdlib>
-#include <ctime>
+
 
 using namespace std;
 
+void displayPattern(int n) {
+
+}
+
+int getType(int n){
+    return 1;
+}
+
+
+double problem3(){
+    return 1;
+}
+
+// P4
+
+
+int countDigitOccurrences(int num, int digit) {
+    return 1;
+}
+void findDigitFrequency(int number) {
+
+}
+
+// p5
+
+void digitToWord(int n){
+
+}
+void numberToWords(int n){
+
+}
+
+
+//p6
+
+//p7
+
+void solveEquation(double a, double b, double c, double d, double e, double f, double& x, double& y, bool& isSolvable) {
+
+}
+
+//p8
+void inputCoordinates(double& x1, double& y1, double& x2, double& y2,
+                      double& x3, double& y3, double& x4, double& y4) {
+
+}
+
+// Function to check if vectors AB and CD are orthogonal using dot product
+bool areVectorsOrthogonal(double x1, double y1, double x2, double y2,
+                          double x3, double y3, double x4, double y4) {
+    return 1;
+}
+
+// p9
+int octal2Dec(const int& octalNumber) {
+    return 1;
+}
+
+
+void revChar(char& c) {
+
+}
+
+double avgOfEvenPlaces(int n) {
+    return 1;
+}
+
+//p12
+
+// Function to approximate the square root of a number
+double sqrt(double n) {
+    return 1;
+}
+
+//p13
+
+int enc(char a, int k);
+
+//p14
+
+int enc(char a, int k){
+    return 1;
+}
+
+// p15
+
+bool chkkaprekar(int n) {
+    return 1;
+}
+
+//p16
+
+long long int numReverse(long long int number) {
+    return 1;
+}
+bool is_Palindrome(long long int num) {
+    return 1;
+}
+bool isLychrel(int num, const int iterCount = 500) {
+    return 1;
+}
+
+// p17
+
+// Function to calculate the number of digits in a number
+int countDigits(int num) {
+    return 1;
+}
+
+// Function to check if a number is a Narcissistic number
+bool isNarcissistic(int num) {
+    return 1;
+}
+
+// Function to find and display the first N Narcissistic numbers
+void findNarcissisticNumbers(int N) {
+
+}
+
+// p18
+
+double year_to_century(int y) {
+    return 1;
+}
+
+// p19
+
+
+int timeToMinutes(int hours, int minutes) {
+    return 1;
+}
+
+// Function to calculate the absolute difference in minutes between two times
+int calculateTimeDifference(int hours1, int minutes1, int hours2, int minutes2) {
+    return 1;
+}
+
+// p20
+
+// Function to check if a year is a leap year
+bool isLeapYear(int year) {
+    return 1;
+}
+
+// Function to calculate the number of days in a month
+int daysInMonth(int month, int year) {
+    return 1;
+}
+
+// Function to calculate the total number of days from the beginning of the year to a given date
+
+
+// Function to calculate the difference in days between two dates
+int daysBetweenDates(int day1, int month1, int year1, int day2, int month2, int year2) {
+    return 1;
+}
+
+
 int main() {
-    cout << "PROBLEM 1\n";
 
-    cout << "PROBLEM 2\n";
+    // TEST YOUR FUNCTIONS IN HERE
 
-    cout << "PROBLEM 3\n";
-
-    cout << "PROBLEM 4\n";
-
-    cout << "PROBLEM 5\n";
-
-    cout << "PROBLEM 6\n";
-
-    cout << "PROBLEM 7\n";
-
-    cout << "PROBLEM 8\n";
-
-    cout << "PROBLEM 9\n";
-
-    cout << "PROBLEM 10\n";
-
-    cout << "PROBLEM 11\n";
-
-    cout << "PROBLEM 12\n";
-
-    cout << "PROBLEM 13\n";
-
-    cout << "PROBLEM 14\n";
-
-    cout << "PROBLEM 15\n";
-
-    cout << "PROBLEM 16\n";
-
-    cout << "PROBLEM 17\n";
-
-    cout << "PROBLEM 18\n";
-
-    cout << "PROBLEM 19\n";
-
-    cout << "PROBLEM 20\n";
-
-    cout << "PROBLEM 21\n";
-
-    cout << "PROBLEM 22\n";
-
-    cout << "PROBLEM 23\n";
-
-    cout << "PROBLEM 24\n";
-
-    cout << "PROBLEM 25\n";
 
     return 0;
 }
